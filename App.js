3be14671097beb2eb132166d52bfb86ebda53353
Jtan <jtan0895@gmail.com>
2022-07-01 19:57:11 +0800

    Updated Flatlist

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, FlatList } from 'react-native';
+import { names } from "./names";
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  //return (
+    //<ScrollView contentContainerStyle={styles.container}>
+    // {names.map((name) => (
+    //    <Text>{name}</Text>
+    //  ))}
+      
+    //</ScrollView>
+
+    const namesAsObjects = names.map((item) =>
+    {
+      return { name: item};
+    }
+    );
+  //);
+  const renderName = ({ item }) => {
+    return <Text>{item.name}</Text>;
+  };
+  return <FlatList 
+    contentContainerStyle={styles.containerText}
+    data={namesAsObjects} 
+    renderItem = {renderName}
+    keyExtractor={(item) => item.name}
+    
+    />;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerText: {
+    margin: 50,
+    marginTop: 50,
+    
+    backgroundColor: 'blue',
+
+  },
+
 });
